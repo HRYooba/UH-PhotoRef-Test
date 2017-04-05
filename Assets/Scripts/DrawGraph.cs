f3d5d23b5d03fa4d804792fc73cb97de5e1009fd
大場洋哉 <HRYooba@gmail.com>
2017-04-05 19:47:30 +0900

    update

--- Assets/Scripts/DrawGraph.cs
+++ Assets/Scripts/DrawGraph.cs
@@ -6,14 +6,21 @@ using Unicessing;
 public class DrawGraph : UGraphics
 {
 
-	public UH uh;
+    public UH uh;
 
     protected override void Setup()
     {
-		uh = GameObject.Find("UnlimitedHand").GetComponent<UH>();
+
     }
     protected override void Draw()
     {
-		
+		translate(3, 0);
+        for (int i = 0; i < 8; i++)
+        {
+            fill(0, 255, 0);
+            rect(i + 0.3f, 0, 0.5f, uh.UHPR[i] / 100.0f);
+			textSize(0.5f);
+			text("" + i, i + 0.3f, -0.5f);
+        }
     }
 }
